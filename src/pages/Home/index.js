c071a174f044aad968ee5fc329bdf6c41215ee93
Henrique Silva de Freitas <69264667+hsfreitas92@users.noreply.github.com>
2023-01-16 18:26:48 -0300

    links social home | deploy netlify

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,43 +1,103 @@
 import './home.css'
+import { useState, useEffect } from 'react'
 import { Social } from '../../components/Social'
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
 
-export default function Home(){
-  return(
+import { toast } from 'react-toastify'
+
+import { db } from '../../services/firebaseConnection'
+import {
+  getDocs,
+  collection,
+  orderBy,
+  query,
+  doc,
+  getDoc
+} from 'firebase/firestore'
+
+export default function Home() {
+  const [links, setLinks] = useState([])
+  const [socialLinks, setSocialLinks] = useState({})
+
+  useEffect(() => {
+    function loadLinks() {
+      const linksRef = collection(db, 'links')
+      const queryRef = query(linksRef, orderBy('created', 'asc'))
+      getDocs(queryRef)
+        .then((snapshot) => {
+          let lista = []
+
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              name: doc.data().name,
+              url: doc.data().url,
+              bg: doc.data().bg,
+              color: doc.data().color
+            })
+          })
+          setLinks(lista);
+        })
+        .catch((error) => {
+          console.log('Erro ao carregar links: ' + error)
+          toast.error('Ops, erro ao carregar o links!')
+        })
+    }
+    loadLinks();
+  }, [])
+
+  useEffect(() => {
+    function loadSocialLinks() {
+      const docRef = doc(db, 'social', 'link')
+      getDoc(docRef)
+        .then((snapshot) => {
+          if (snapshot.data() !== undefined) {
+            setSocialLinks({
+              facebook: snapshot.data().facebook,
+              instagram: snapshot.data().instagram,
+              youtube: snapshot.data().youtube
+            })
+          }
+        })
+        .catch((error) => {
+          console.log('Erro ao carregar social: ' + error)
+          toast.error('Ops, erro ao carregar o social!')
+        })
+    }
+    loadSocialLinks();
+  }, [])
+
+  return (
     <div className="home-container">
       <h1>Sujeito prog</h1>
       <span>Veja meus links 👇 </span>
 
       <main className="links">
-        <session className="link-area">
-          <a href="#">
-            <p className="link-text">Canal youtube</p>
-          </a>
-        </session>
 
-        <session className="link-area">
-          <a href="#">
-            <p className="link-text">Grupo telegram</p>
-          </a>
-        </session>
+        {links.map((item) => (
+          <session key={item.id} className="link-area" style={{ backgroundColor: item.bg }}>
+            <a href={item.url} target='blank'>
+              <p className="link-text" style={{ color: item.color }}>
+                {item.name}
+              </p>
+            </a>
+          </session>
+        ))}
 
-        <session className="link-area">
-          <a href="#">
-            <p className="link-text">Treinamento</p>
-          </a>
-        </session>
+        {links.length !== 0 && Object.keys(socialLinks).length > 0 && (
+          <footer>
+            <Social url={socialLinks?.facebook}>
+              <FaFacebook size={35} color="#FFF" />
+            </Social>
+            <Social url={socialLinks?.youtube}>
+              <FaYoutube size={35} color="#FFF" />
+            </Social>
+            <Social url={socialLinks?.instagram}>
+              <FaInstagram size={35} color="#FFF" />
+            </Social>
+          </footer>
+        )}
 
-        <footer>
-          <Social url="https://facebook.com">
-            <FaFacebook size={35} color="#FFF" />
-          </Social>
-          <Social url="https://youtube.com">
-            <FaYoutube size={35} color="#FFF" />
-          </Social>
-          <Social url="https://instagram.com">
-            <FaInstagram size={35} color="#FFF" />
-          </Social>
-        </footer>
       </main>
     </div>
   )
